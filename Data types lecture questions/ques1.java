5ef9708c1a40878828a091c8a9b58160f4f120c6
Kapoorrashi <46554050+Kapoorrashi@users.noreply.github.com>
2019-01-16 11:45:39 +0530

    Add files via upload

---
+++ Data types lecture questions/ques1.java
@@ -0,0 +1,9 @@
+public class ques1 {
+	public static void main(String [] args) {
+	int a,b,sum=0;
+	a=10;
+	b=10;
+	sum=a+b;
+	System.out.println("The sum is-" +sum);
+	}
+}
---
+++ Data types lecture questions/ques2.java
@@ -0,0 +1,9 @@
+public class ques2 {
+	public static void main(String[] args) {
+	char r;
+	int i;
+	i = 'r';
+	System.out.println(i);
+
+	}
+}
