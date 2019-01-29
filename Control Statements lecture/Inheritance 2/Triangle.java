e8aa0ed44c70621459aff97f5c481201db15b251
Kapoorrashi <46554050+Kapoorrashi@users.noreply.github.com>
2019-01-29 10:08:58 +0530

    Add files via upload

---
+++ Control Statements lecture/Inheritance 2/Triangle.java
@@ -0,0 +1,10 @@
+class Triangle extends Shape {  
+ 
+ Triangle(double a, double b) { 
+     super(a, b);   } 
+ 
+  double display_area() { 
+      System.out.println("Area for Triangle.");  
+         return d1 * d2 / 2; 
+     }
+}     
